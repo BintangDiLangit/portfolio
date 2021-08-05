c328719ac6f9a3b31268daba6aa354a1a298c41d
bintangdilangit <syekhuda463@gmail.com>
2021-08-06 05:16:34 +0700

    install dompdf

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Barryvdh\DomPDF\Facade as PDF;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\App;
 
 class UserController extends Controller
 {
@@ -15,7 +17,21 @@ class UserController extends Controller
     public function show($id){
         $timeNow = Carbon::now()->format('d M, Y');
         $user = User::where('id', $id)->first();
-        return view('appreciation.certificate',compact('timeNow','user'));
+
+        $contxt = stream_context_create([
+            'ssl' => [
+            'verify_peer' => FALSE,
+            'verify_peer_name' => FALSE,
+            'allow_self_signed'=> TRUE
+            ]
+            ]);
+            $pdf = PDF::setOptions(['isHtml5ParserEnabled' => true, 'isRemoteEnabled' => true]);
+            $pdf->getDomPDF()->setHttpContext($contxt);
+            $pdf = App::make('dompdf.wrapper');
+            $pdf->loadHTML(view('appreciation.certificate', compact('timeNow','user')));
+            return $pdf->stream();
+        // return PDF::setOptions(['isHtml5ParserEnabled' => true, 'isRemoteEnabled' => true])->loadView('appreciation.certificate', compact('timeNow','user'))->stream();
+        // return view('appreciation.certificate',compact('timeNow','user'));
     }
 
 
