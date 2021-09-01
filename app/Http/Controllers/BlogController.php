b7550f42f84044dad9517f1ea96c66a38a1d4287
bintangdilangit <syekhuda463@gmail.com>
2021-09-01 07:52:32 +0700

    fix bug blog controller

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -13,7 +13,7 @@ class BlogController extends Controller
     {
         $blog = Blog::orderBy('created_at', 'desc')->get();
         $blogUser = Blog::where('user_id', Auth::user()->id)->get();
-        return view('admin.blog.index', compact('blog','blogUser'));
+        return view('admin.blog.index', compact('blog', 'blogUser'));
     }
 
     public function create()
@@ -24,21 +24,22 @@ class BlogController extends Controller
     public function store(Request $request)
     {
         $this->validate(request(), [
-            'title'    => 'required|string|regex:/^[a-zA-Z]+$/u',
+            'title'    => 'required|string|max:255',
             'imageHeader'    => 'required|image',
             'content'    => 'required',
         ]);
         $a = strtolower($request->title);
-        $b = str_replace(' ','-',$a);
+        $b = str_replace(' ', '-', $a);
+        $c = preg_replace('/[^A-Za-z0-9\-]/', '', $b);
 
         $tags_arr = explode(',', strtolower($request["tags"]));
 
         $tag_ids = [];
-        foreach($tags_arr as $tag_name){
+        foreach ($tags_arr as $tag_name) {
             $tag = Tag::where('tag_name', $tag_name)->first();
             if ($tag) {
                 $tag_ids[] = $tag->id;
-            } else{
+            } else {
                 $new_tag = Tag::create([
                     'tag_name' => $tag_name
                 ]);
@@ -52,15 +53,15 @@ class BlogController extends Controller
             $blg->content = $request->content;
             $blg->creator = Auth::user()->name;
             $blg->user_id = Auth::user()->id;
-            $blg->link_route = $b;
-            $request->file('imageHeader')->move('blog-images/',$request->file('imageHeader')->getClientOriginalName());
+            $blg->link_route = $c;
+            $request->file('imageHeader')->move('blog-images/', $request->file('imageHeader')->getClientOriginalName());
             $blg->imageHeader = $request->file('imageHeader')->getClientOriginalName();
             if ($blg->save()) {
                 $blg->tags()->attach($tag_ids);
             }
         }
 
-        return redirect(route('blog.index'))->with('success','Blog has been created');
+        return redirect(route('blog.index'))->with('success', 'Blog has been created');
     }
 
     public function edit($id)
@@ -72,29 +73,30 @@ class BlogController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate(request(), [
-            'title'    => 'required|string',
+            'title'    => 'required|string|max:255',
             'imageHeader'    => 'nullable|image',
             'content'    => 'required',
         ]);
         $a = strtolower($request->title);
-        $b = str_replace(' ','-',$a);
+        $b = str_replace(' ', '-', $a);
+        $c = preg_replace('/[^A-Za-z0-9\-]/', '', $b);
 
         $blg = Blog::find($id);
         $blg->title = $request->title;
-        $blg->link_route = $b;
+        $blg->link_route = $c;
         $blg->content = $request->content;
         if ($request->hasFile('imageHeader')) {
-            $request->file('imageHeader')->move('blog-images/',$request->file('imageHeader')->getClientOriginalName());
+            $request->file('imageHeader')->move('blog-images/', $request->file('imageHeader')->getClientOriginalName());
             $blg->imageHeader = $request->file('imageHeader')->getClientOriginalName();
         }
         $blg->update();
-        return redirect(route('blog.index'))->with('success','Blog has been updated');
+        return redirect(route('blog.index'))->with('success', 'Blog has been updated');
     }
 
     public function destroy($id)
     {
         $blg = Blog::find($id);
         $blg->delete();
-        return redirect(route('blog.index'))->with('success','Blog has been deleted');
+        return redirect(route('blog.index'))->with('success', 'Blog has been deleted');
     }
 }
