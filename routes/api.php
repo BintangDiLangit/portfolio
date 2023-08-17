1c51446bcd995ccf61af269abdd0b1b8f96e1c53
Bintang Miftaqul Huda <56743346+BintangDiLangit@users.noreply.github.com>
2023-08-17 15:51:56 +0700

    add api awardee

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\api\CertificateController;
 use App\Http\Controllers\api\ClientController;
+use App\Http\Controllers\api\CompetitionController;
 use App\Http\Controllers\api\CVController;
 use App\Http\Controllers\api\PortfolioController;
 use App\Http\Controllers\api\SEOController;
@@ -13,6 +14,9 @@ Route::prefix('v1')->group(function () {
 
     Route::post('/all-portfolios', [PortfolioController::class, 'index']);
     Route::post('/portfolio/{id}', [PortfolioController::class, 'show']);
+
+    Route::post('/all-awardees', [CompetitionController::class, 'index']);
+    Route::post('/awardee/{id}', [CompetitionController::class, 'show']);
 
     Route::post('/seo', [SEOController::class, 'index']);
     Route::post('/skills', [SkillController::class, 'index']);
